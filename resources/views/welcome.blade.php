590320f308f1548d8cf9078415828c579845b3c6
glennmckimm <glenn_mckimm@hotmail.co.uk>
2022-01-11 10:36:52 +0000

    testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,6 +113,20 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="p-6 border-t border-gray-200 dark:border-gray-700" style="grid-column: span 2 / span 2;">
+                            <div class="flex items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
+                                    <path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                                </svg>
+                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Hello Enterprise Projects!</div>
+                            </div>
+
+                            <div class="ml-12">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                    Casual copy pasta
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
